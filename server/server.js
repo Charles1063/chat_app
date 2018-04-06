e3ce0ad31de3a95ce25e77545e9c2df617903539
Cody Fang <fangce1063@gmail.com>
2018-04-06 16:01:05 -0400

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -22,16 +22,35 @@ io.on('connection', (socket) => {
   //   createAt: 123123
   // });
 
-socket.on('createMessage', (message) => {
-  console.log('createMessage', message);
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    // emit to every single connection
+    io.emit('newMessage', {
+      from: message.from,
+      text:message.text,
+      createAt: new Date().getTime()
+    });
+  });
 
-  // emit to every single connection
-  io.emit('newMessage', {
-    from: message.from,
-    text:message.text,
+  //socket.emit from admin text welcome to the chat app
+  socket.emit('newMessage', {
+    from:'Admin',
+    text:'Welcome to the chat app',
     createAt: new Date().getTime()
   });
-});
+  // socket.broadccast.emit from admin text new user joined
+  socket.broadcast.emit('newMessage', {
+    from:'Admin',
+    test:'New user joined',
+    createAt: new Date().getTime()
+  });
+  // //send to everybudy except this socket
+  // socket.broadcast.emit('newMessage',, {
+  //   from: message.from,
+  //   text:message.text,
+  //   createAt: new Date().getTime()
+  // });
+// });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');

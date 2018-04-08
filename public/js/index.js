fd00501bccaf4d1a86beced24930c63e33cb6733
Cody Fang <fangce1063@gmail.com>
2018-04-08 09:33:11 -0400

    croll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,25 @@
 var socket = io();
 
+function scrollToButtom () {
+  // selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  // height
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+    
+
+  }
+};
+
+
 socket.on('connect', function () {
   console.log('Connected to server');
 
@@ -28,7 +48,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
-
+  scrollToButtom();
 });
 
 // socket.emit('createMessage', {
@@ -48,6 +68,7 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToButtom();
   // var li = jQuery('<li></li>');
   // //open a new tab for google map infomation
   // var a = jQuery('<a target="_blank">My current location</a>');
